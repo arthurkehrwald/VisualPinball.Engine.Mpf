2ac3ee21dafef9a562ba242ab9ba05c324733e6c
freezy <freezy@kodi.tv>
2020-10-16 00:19:54 +0200

    Initial commit.

---
+++ MpfLib/MpfClient.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using Mpf;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MpfLib
+{
+	public class MpfClient : IDisposable
+	{
+		private Channel _channel;
+
+		public async Task<MpfClient> Connect(string ipPort = "localhost:50051") {
+			Console.WriteLine($"Connecting to {ipPort}...");
+			_channel = new Channel(ipPort, ChannelCredentials.Insecure);
+			await _channel.ConnectAsync();
+			return this;
+		}
+
+		public async Task<IEnumerable<string>> KnownCoils() {
+			AssertConnected();
+			var client = new HardwarePlatform.HardwarePlatformClient(_channel);
+			var details = await client.GetPlatformDetailsAsync(new GetPlatformDetailsRequest());
+			return details.KnownCoils;
+		}
+
+		public void Dispose()
+		{
+			Console.WriteLine("Disconnecting...");
+			_channel.ShutdownAsync().Wait();
+			Console.WriteLine("Done!");
+		}
+
+		private void AssertConnected() {
+			if (_channel == null) {
+				throw new InvalidOperationException("Must .Connect() first!");
+			}
+		}
+	}
+}
---
+++ MpfTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using MpfLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpfTest
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var client = new MpfClient();
+			using (await client.Connect()) {
+				Console.WriteLine("Known Coils: ");
+				foreach (var coil in await client.KnownCoils())
+				{
+					Console.WriteLine("  - " + coil);
+				}
+			}
+		}
+	}
+}
