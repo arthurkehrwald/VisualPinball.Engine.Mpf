bbd3c3bc88525235e2165557d5974d0230e331dc
freezy <freezy@kodi.tv>
2021-03-08 00:22:31 +0100

    client: Dispatch commands (still blocking).

--- VisualPinball.Engine.Mpf/MpfClient.cs
+++ VisualPinball.Engine.Mpf/MpfClient.cs
@@ -13,8 +13,9 @@ using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Google.Protobuf.Collections;
+using Grpc.Core.Utils;
 using Mpf.Vpe;
+using NLog;
 
 namespace VisualPinball.Engine.Mpf
 {
@@ -23,11 +24,22 @@ namespace VisualPinball.Engine.Mpf
 	/// </summary>
 	public class MpfClient
 	{
+		public event EventHandler<FadeLightRequest> OnFadeLight;
+		public event EventHandler<PulseCoilRequest> OnPulseCoil;
+		public event EventHandler<EnableCoilRequest> OnEnableCoil;
+		public event EventHandler<DisableCoilRequest> OnDisableCoil;
+		public event EventHandler<ConfigureHardwareRuleRequest> OnConfigureHardwareRule;
+		public event EventHandler<RemoveHardwareRuleRequest> OnRemoveHardwareRule;
+
 		private Channel _channel;
 		private MpfHardwareService.MpfHardwareServiceClient _client;
 
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
 		public void Connect(string serverIpPort = "127.0.0.1:50051")
 		{
+			Logger.Info($"Connecting to {serverIpPort}...");
 			_channel = new Channel(serverIpPort, ChannelCredentials.Insecure);
 			_client = new MpfHardwareService.MpfHardwareServiceClient(_channel);
 		}
@@ -38,11 +50,50 @@ namespace VisualPinball.Engine.Mpf
 			foreach (var sw in initialSwitches.Keys) {
 				ms.InitialSwitchStates.Add(sw, initialSwitches[sw]);
 			}
-			_client.Start(ms);
+
+			Logger.Info("Starting client...");
+			using (var call = _client.Start(ms)) {
+
+				Logger.Info("Client started, retrieving commands...");
+				var count = 0;
+				call.ResponseStream.ForEachAsync(commands => {
+
+					Logger.Info($"New command: {commands.CommandCase}");
+					count++;
+					switch (commands.CommandCase) {
+						case Commands.CommandOneofCase.None:
+							break;
+						case Commands.CommandOneofCase.FadeLight:
+							OnFadeLight?.Invoke(this, commands.FadeLight);
+							break;
+						case Commands.CommandOneofCase.PulseCoil:
+							OnPulseCoil?.Invoke(this, commands.PulseCoil);
+							break;
+						case Commands.CommandOneofCase.EnableCoil:
+							OnEnableCoil?.Invoke(this, commands.EnableCoil);
+							break;
+						case Commands.CommandOneofCase.DisableCoil:
+							OnDisableCoil?.Invoke(this, commands.DisableCoil);
+							break;
+						case Commands.CommandOneofCase.ConfigureHardwareRule:
+							OnConfigureHardwareRule?.Invoke(this, commands.ConfigureHardwareRule);
+							break;
+						case Commands.CommandOneofCase.RemoveHardwareRule:
+							OnRemoveHardwareRule?.Invoke(this, commands.RemoveHardwareRule);
+							break;
+						default:
+							throw new ArgumentOutOfRangeException();
+					}
+					return Task.CompletedTask;
+				}).Wait();
+
+				Logger.Info($"{count} commands dispatched.");
+			}
 		}
 
 		public MachineDescription GetMachineDescription()
 		{
+			Logger.Info($"Getting machine description...");
 			return _client.GetMachineDescription(new EmptyRequest());
 		}
 
