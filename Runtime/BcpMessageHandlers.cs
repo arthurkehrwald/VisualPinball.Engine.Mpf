70ab7aff3c2a698873392be2e54782fac5a4b5fb
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-10-17 20:19:08 +0200

    Make sure message handlers are initialized

--- Runtime/BcpMessageHandlers.cs
+++ Runtime/BcpMessageHandlers.cs
@@ -11,5 +11,16 @@ namespace FutureBoxSystems.MpfMediaController
         public BcpMessageHandler<HelloMessage> Hello => hello ??= new(HelloMessage.Command, HelloMessage.FromGenericMessage, bcpInterface);
         private BcpMessageHandler<GoodbyeMessage> goodbye;
         public BcpMessageHandler<GoodbyeMessage> Goodbye => goodbye ??= new(GoodbyeMessage.Command, GoodbyeMessage.FromGenericMessage, bcpInterface);
+
+        private void Awake()
+        {
+            // Initialize command handlers
+            // Lazy initialization using the property getters above is needed because
+            // event handlers may try to access them before this Awake function is called.
+            // Even if no event handlers subscribe, the message handlers still need to exist,
+            // because an error message is sent for each command for which no handler exists.
+            _ = Hello;
+            _ = Goodbye;
+        }
     }
 }
