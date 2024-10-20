f6dc914cfe0de7b72a742807b14694285e2c4d0d
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-10-20 22:27:48 +0200

    Add reverse mapping to string enum

--- Runtime/Utils/StringEnum.cs
+++ Runtime/Utils/StringEnum.cs
@@ -1,5 +1,5 @@
-﻿// Enum to string mapping
-// Source: https://weblogs.asp.net/stefansedich/enum-with-string-values-in-c
+﻿// Maps enum values to arbitrary strings and back using a custom attribute
+// Based on: https://weblogs.asp.net/stefansedich/enum-with-string-values-in-c
 
 using System;
 using System.Reflection;
@@ -17,14 +17,36 @@ namespace FutureBoxSystems.MpfMediaController
         }
     }
 
-    public static class EnumExtension
+    public static class StringEnum
     {
         public static string GetStringValue(this Enum value)
         {
             Type type = value.GetType();
             FieldInfo fieldInfo = type.GetField(value.ToString());
             StringValueAttribute attribute = fieldInfo.GetCustomAttribute<StringValueAttribute>();
-            return attribute.StringValue ?? null;
+            return attribute.StringValue;
+        }
+
+        public static T GetValueFromString<T>(string value) where T : Enum
+        {
+            Type enumType = typeof(T);
+            if (!enumType.IsEnum)
+            {
+                throw new ArgumentException($"{enumType} is not an enum type.");
+            }
+
+            foreach (var field in enumType.GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(StringValueAttribute)) is StringValueAttribute attribute)
+                {
+                    if (attribute.StringValue == value)
+                    {
+                        return (T)field.GetValue(null);
+                    }
+                }
+            }
+
+            throw new ArgumentException($"No enum value with the string value '{value}' found.");
         }
     }
 }
