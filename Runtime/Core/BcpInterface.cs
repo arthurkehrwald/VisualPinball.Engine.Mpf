6ac4127d7b8503365ff2e55f2102cc5128ad1b39
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-10-27 17:00:41 +0100

    Only monitor machine vars when necessary

--- Runtime/Core/BcpInterface.cs
+++ Runtime/Core/BcpInterface.cs
@@ -99,7 +99,6 @@ namespace FutureBoxSystems.MpfMediaController
                     if (userCount > 0)
                         TrySendMessage(new MonitorStartMessage(category));
                 }
-                TrySendMessage(new MonitorStartMessage(MonitoringCategory.MachineVars));
             }
         }
 
