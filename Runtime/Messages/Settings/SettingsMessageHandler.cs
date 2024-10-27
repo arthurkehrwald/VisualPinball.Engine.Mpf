601e1f82bf263620869a16da1954fd527b5807e3
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-10-27 20:13:30 +0100

    Add monitoring category to settings message handler

--- Runtime/Messages/Settings/SettingsMessageHandler.cs
+++ Runtime/Messages/Settings/SettingsMessageHandler.cs
@@ -1,8 +1,11 @@
+using FutureBoxSystems.MpfMediaController.Messages.Monitor;
+
 namespace FutureBoxSystems.MpfMediaController.Messages.Settings
 {
     public class SettingsMessageHandler : BcpMessageHandler<SettingsMessage>
     {
         public override string Command => SettingsMessage.Command;
         protected override ParseDelegate Parse => SettingsMessage.FromGenericMessage;
+        public override MonitoringCategory MonitoringCategory => MonitoringCategory.MachineVars;
     }
 }
