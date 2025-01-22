a198edcd3015f207e97f8ec0ab52aed29a897f6f
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2025-01-22 23:18:05 +0100

    Port inspector UI to UiToolkit

--- Editor/MpfGamelogicEngineInspector.cs
+++ Editor/MpfGamelogicEngineInspector.cs
@@ -11,203 +11,150 @@
 
 // ReSharper disable AssignmentInConditionalExpression
 
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 using VisualPinball.Unity;
-using VisualPinball.Unity.Editor;
 
 namespace VisualPinball.Engine.Mpf.Unity.Editor
 {
     [CustomEditor(typeof(MpfGamelogicEngine))]
     public class MpfGamelogicEngineInspector : UnityEditor.Editor
     {
-        private MpfGamelogicEngine _mpfEngine;
-        private TableComponent _tableComponent;
-        private SerializedProperty _consoleOptionsProperty;
+        [SerializeField]
+        private VisualTreeAsset _inspectorXml;
 
-        private bool _foldoutSwitches;
-        private bool _foldoutCoils;
-        private bool _foldoutLamps;
-
-        private bool HasData =>
-            _mpfEngine.RequestedSwitches.Length
-                + _mpfEngine.RequestedCoils.Length
-                + _mpfEngine.RequestedLamps.Length
-            > 0;
-
-        private void OnEnable()
+        public override VisualElement CreateInspectorGUI()
         {
-            _mpfEngine = target as MpfGamelogicEngine;
-            if (_mpfEngine != null)
-            {
-                _tableComponent = _mpfEngine.gameObject.GetComponentInParent<TableComponent>();
-            }
-            _consoleOptionsProperty = serializedObject.FindProperty("_mpfArguments");
-        }
+            var root = _inspectorXml.Instantiate();
+            var mpfEngine = (MpfGamelogicEngine)serializedObject.targetObject;
+            var tableComponent = mpfEngine.GetComponentInParent<TableComponent>();
 
-        public override void OnInspectorGUI()
-        {
-            if (!_tableComponent)
-            {
-                EditorGUILayout.HelpBox(
-                    $"Cannot find table. The gamelogic engine must be applied to a table object or one of its children.",
-                    MessageType.Error
-                );
-                return;
-            }
+            var machineFolderField = root.Q<TextField>("machine-folder");
+            var machineFolderInput = machineFolderField.Q(name: "unity-text-input");
+            machineFolderInput.RegisterCallback<ClickEvent>(
+                (evt) =>
+                {
+                    if (!Directory.Exists(Application.streamingAssetsPath))
+                    {
+                        Directory.CreateDirectory(Application.streamingAssetsPath);
+                    }
+                    var openFolder = Directory.Exists(mpfEngine.MachineFolder)
+                        ? mpfEngine.MachineFolder
+                        : Application.streamingAssetsPath;
+                    var path = EditorUtility.OpenFolderPanel(
+                        "Mission Pinball Framework: Choose machine folder",
+                        openFolder,
+                        ""
+                    );
+                    if (!string.IsNullOrWhiteSpace(path))
+                    {
+                        if (path.Contains("StreamingAssets/"))
+                            path = "./StreamingAssets/" + path.Split("StreamingAssets/")[1];
 
-            if (
-                !string.IsNullOrEmpty(_mpfEngine.MachineFolder)
-                && !_mpfEngine.MachineFolder.Contains("StreamingAssets")
-            )
-            {
-                EditorGUILayout.HelpBox(
-                    "The machine folder is not in the 'StreamingAssets' folder. It will not be included in builds.",
-                    MessageType.Warning
-                );
-            }
-
-            EditorGUI.BeginChangeCheck();
-            EditorGUILayout.PropertyField(
-                _consoleOptionsProperty,
-                new GUIContent("Console Options")
+                        var machineFolderProp = serializedObject.FindProperty(
+                            nameof(MpfGamelogicEngine._machineFolder)
+                        );
+                        machineFolderProp.stringValue = path;
+                        serializedObject.ApplyModifiedProperties();
+                    }
+                }
             );
-            if (EditorGUI.EndChangeCheck())
+
+            var getDescBtn = root.Q<Button>("get-machine-description");
+            getDescBtn.clicked += () =>
             {
-                serializedObject.ApplyModifiedProperties();
-            }
+                Undo.RecordObject(mpfEngine, "Get machine description");
+                PrefabUtility.RecordPrefabInstancePropertyModifications(mpfEngine);
+                mpfEngine.QueryParseAndStoreMpfMachineDescription();
+            };
 
-            var pos = EditorGUILayout.GetControlRect(true, 18f);
-            pos = EditorGUI.PrefixLabel(pos, new GUIContent("Machine Folder"));
-
-            if (GUI.Button(pos, _mpfEngine.MachineFolder, EditorStyles.objectField))
-            {
-                if (!Directory.Exists(Application.streamingAssetsPath))
-                {
-                    Directory.CreateDirectory(Application.streamingAssetsPath);
-                }
-                var openFolder = Directory.Exists(_mpfEngine.MachineFolder)
-                    ? _mpfEngine.MachineFolder
-                    : Application.streamingAssetsPath;
-                var path = EditorUtility.OpenFolderPanel(
-                    "Mission Pinball Framework: Choose machine folder",
-                    openFolder,
-                    ""
-                );
-                if (!string.IsNullOrWhiteSpace(path))
-                {
-                    _mpfEngine.MachineFolder = path;
-                }
-            }
-
-            if (GUILayout.Button("Get Machine Description"))
-            {
-                if (!Directory.Exists(_mpfEngine.MachineFolder))
-                {
-                    EditorUtility.DisplayDialog(
-                        "Mission Pinball Framework",
-                        "Gotta choose a valid machine folder first!",
-                        "Okay"
-                    );
-                }
-                else if (!Directory.Exists(Path.Combine(_mpfEngine.MachineFolder, "config")))
-                {
-                    EditorUtility.DisplayDialog(
-                        "Mission Pinball Framework",
-                        $"{_mpfEngine.MachineFolder} doesn't seem a valid machine folder. We expect a \"config\" subfolder in there!",
-                        "Okay"
-                    );
-                }
-                else
-                {
-                    Undo.RecordObject(_mpfEngine, "Get machine description");
-                    PrefabUtility.RecordPrefabInstancePropertyModifications(_mpfEngine);
-                    _mpfEngine.QueryParseAndStoreMpfMachineDescription();
-                }
-            }
-
-            EditorGUI.BeginDisabledGroup(!HasData);
-            if (GUILayout.Button("Populate Hardware"))
+            var repopulateHardwareBtn = root.Q<Button>("populate-hardware");
+            repopulateHardwareBtn.clicked += () =>
             {
                 if (
                     EditorUtility.DisplayDialog(
                         "Mission Pinball Framework",
-                        "This will clear all linked switches, coils and lamps and re-populate them. You sure you want to do that?",
+                        "This will clear all linked switches, coils and lamps and re-populate them. Are you sure you want to do that?",
                         "Yes",
                         "No"
                     )
                 )
                 {
-                    _tableComponent.RepopulateHardware(_mpfEngine);
+                    Undo.RecordObject(tableComponent, "Populate hardware");
+                    PrefabUtility.RecordPrefabInstancePropertyModifications(tableComponent);
+                    tableComponent.RepopulateHardware(mpfEngine);
                     TableSelector.Instance.TableUpdated();
                     SceneView.RepaintAll();
                 }
-            }
-            EditorGUI.EndDisabledGroup();
+            };
 
-            var naStyle = new GUIStyle(GUI.skin.label) { fontStyle = FontStyle.Italic };
+            var switchesProp = serializedObject.FindProperty(
+                nameof(MpfGamelogicEngine._requestedSwitches)
+            );
+            var switchFoldout = root.Q<Foldout>("switches");
+            UpdateSwitchList(mpfEngine, switchFoldout);
+            switchFoldout.TrackPropertyValue(
+                switchesProp,
+                (prop) => UpdateSwitchList(mpfEngine, switchFoldout)
+            );
 
-            // list switches, coils and lamps
-            if (
-                _mpfEngine.RequestedCoils.Length
-                    + _mpfEngine.RequestedSwitches.Length
-                    + _mpfEngine.RequestedLamps.Length
-                > 0
-            )
+            var coilsProp = serializedObject.FindProperty(
+                nameof(MpfGamelogicEngine._requestedCoils)
+            );
+            var coilFoldout = root.Q<Foldout>("coils");
+            UpdateCoilList(mpfEngine, coilFoldout);
+            coilFoldout.TrackPropertyValue(
+                coilsProp,
+                (prop) => UpdateCoilList(mpfEngine, coilFoldout)
+            );
+
+            var lampsProp = serializedObject.FindProperty(
+                nameof(MpfGamelogicEngine._requestedLamps)
+            );
+            var lampsFoldout = root.Q<Foldout>("lamps");
+            UpdateLampList(mpfEngine, lampsFoldout);
+            lampsFoldout.TrackPropertyValue(
+                lampsProp,
+                (prop) =>
+                {
+                    IEnumerable<string> ids = mpfEngine.RequestedLamps.Select(lamp => lamp.Id);
+                    UpdateGameItemList(lampsFoldout, ids);
+                }
+            );
+
+            return root;
+        }
+
+        private void UpdateSwitchList(MpfGamelogicEngine mpfEngine, VisualElement parent)
+        {
+            IEnumerable<string> ids = mpfEngine.RequestedSwitches.Select(sw => sw.Id);
+            UpdateGameItemList(parent, ids);
+        }
+
+        private void UpdateCoilList(MpfGamelogicEngine mpfEngine, VisualElement parent)
+        {
+            IEnumerable<string> ids = mpfEngine.RequestedCoils.Select(coil => coil.Id);
+            UpdateGameItemList(parent, ids);
+        }
+
+        private void UpdateLampList(MpfGamelogicEngine mpfEngine, VisualElement parent)
+        {
+            IEnumerable<string> ids = mpfEngine.RequestedLamps.Select(lamp => lamp.Id);
+            UpdateGameItemList(parent, ids);
+        }
+
+        private void UpdateGameItemList(VisualElement parent, IEnumerable<string> itemIds)
+        {
+            parent.Clear();
+            foreach (var id in itemIds)
             {
-                if (
-                    _foldoutSwitches = EditorGUILayout.BeginFoldoutHeaderGroup(
-                        _foldoutSwitches,
-                        "Switches"
-                    )
-                )
-                {
-                    foreach (var sw in _mpfEngine.RequestedSwitches)
-                    {
-                        EditorGUILayout.LabelField(
-                            new GUIContent(
-                                $"  {sw.Id} ",
-                                Icons.Switch(sw.NormallyClosed, IconSize.Small)
-                            )
-                        );
-                    }
-                    if (_mpfEngine.RequestedSwitches.Length == 0)
-                    {
-                        EditorGUILayout.LabelField("No switches in this machine.", naStyle);
-                    }
-                }
-                EditorGUILayout.EndFoldoutHeaderGroup();
-
-                if (_foldoutCoils = EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutCoils, "Coils"))
-                {
-                    foreach (var sw in _mpfEngine.RequestedCoils)
-                    {
-                        EditorGUILayout.LabelField(
-                            new GUIContent($"  {sw.Id} ", Icons.Coil(IconSize.Small))
-                        );
-                    }
-                    if (_mpfEngine.RequestedCoils.Length == 0)
-                    {
-                        EditorGUILayout.LabelField("No coils in this machine.", naStyle);
-                    }
-                }
-                EditorGUILayout.EndFoldoutHeaderGroup();
-
-                if (_foldoutLamps = EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutLamps, "Lamps"))
-                {
-                    foreach (var sw in _mpfEngine.RequestedLamps)
-                    {
-                        EditorGUILayout.LabelField(
-                            new GUIContent($"  {sw.Id} ", Icons.Light(IconSize.Small))
-                        );
-                    }
-                    if (_mpfEngine.RequestedLamps.Length == 0)
-                    {
-                        EditorGUILayout.LabelField("No lamps in this machine.", naStyle);
-                    }
-                }
-                EditorGUILayout.EndFoldoutHeaderGroup();
+                var label = new Label(id);
+                parent.Add(label);
             }
         }
     }
